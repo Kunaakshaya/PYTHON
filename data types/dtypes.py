56e354a76421f96a0da3226738a8937984175aae
Yuvateja <yuvatejareddy003@gmail.com>
2021-08-31 15:26:29 +0530

    data types�

---
+++ data types/dtypes.py
@@ -0,0 +1,21 @@
+st1 = "This is a string"
+print(st1)
+print(f"{st1} type is {type(st1)}")
+int1 = 19
+print(int1)
+print(f"type of {int1} is {type(int1)}")
+f1 = 99.9999
+print(f1)
+print(f"type of {f1} is {type(f1)}")
+l1 = ['A','K','S','H','A','Y','A']
+print(l1)
+print(f"type of {l1} is {type(l1)}")
+t1 = ('A','K','S','H','A','Y','A')
+print(t1)
+print(f"type of {t1} is {type(t1)}")
+set1 = set(l1)
+print(set1)
+print(f"type of {set1} is {type(set1)}")
+dict1 = {"name":"Akshaya","college":"SEC","dept":"CSE", "year":"2019-2023"}
+print(dict1)
+print(f"type of {dict1} is {type(dict1)}")
